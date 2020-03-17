0297865efbbc54fa13872ddcd0f3fb23d10f80da
SilverJun <jangej1031@naver.com>
2020-03-17 15:53:57 +0900

    flutter_layout_demo implement done.

--- flutter_layout_classwork/lib/main.dart
+++ flutter_layout_classwork/lib/main.dart
@@ -31,11 +31,7 @@ class MyApp extends StatelessWidget {
                 ],
               )
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -96,15 +92,15 @@ class MyApp extends StatelessWidget {
       children: <Widget>[
         Icon(icon, color: color),
         Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          )
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            )
         )
       ],
     );
@@ -112,7 +108,50 @@ class MyApp extends StatelessWidget {
 
 }
 
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
 
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          )
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text("$_favoriteCount")
+          )
+        )
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
 
 
 
