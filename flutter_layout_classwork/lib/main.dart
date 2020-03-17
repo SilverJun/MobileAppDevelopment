3e64bb6f9a06c675d4d75cc4b85b84c2718f6517
SilverJun <jangej1031@naver.com>
2020-03-17 16:36:52 +0900

    W3 Classwork done.

--- flutter_layout_classwork/lib/main.dart
+++ flutter_layout_classwork/lib/main.dart
@@ -5,6 +5,55 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      theme: ThemeData(primaryColor: Colors.black),
+      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Flutter layout demo'),
+//        ),
+        body: ListView(
+          children: <Widget>[
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            TitleWidget(),
+            Divider(height: 1.0, color: Colors.black,),
+            ButtonSection(),
+            Divider(height: 1.0, color: Colors.black,),
+            BottomSection(),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
+
+class _TitleWidgetState extends State<TitleWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -16,14 +65,14 @@ class MyApp extends StatelessWidget {
                   Container(
                       padding: const EdgeInsets.only(bottom: 8),
                       child: Text(
-                        'oeschinen Lake Campground',
+                        (_isFavorited?'EUNJUN JANG':'oeschinen Lake Campground'),
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
                       )
                   ),
                   Text(
-                    'Kandersteg, Switzerland',
+                    (_isFavorited?'21800633':'oeschinen Lake Campground'),
                     style: TextStyle(
                       color: Colors.grey[500],
                     ),
@@ -31,60 +80,39 @@ class MyApp extends StatelessWidget {
                 ],
               )
           ),
-          FavoriteWidget(),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.all(0),
+                  child: IconButton(
+                    icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+                    color: Colors.red[500],
+                    onPressed: _toggleFavorite,
+                  )
+              ),
+              SizedBox(
+                  width: 18,
+                  child: Container(
+                      child: Text("$_favoriteCount")
+                  )
+              )
+            ],
+          ),
         ],
       ),
     );
 
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildButtonColumn(color, Icons.call, 'CALL'),
-              _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-              _buildButtonColumn(color, Icons.share, 'SHARE'),
-            ]
-        )
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
-        ),
-      ),
-    );
+    return titleSection;
   }
+}
 
+class TitleWidget extends StatefulWidget {
+  @override
+  _TitleWidgetState createState() => _TitleWidgetState();
+}
+
+class ButtonSection extends StatelessWidget {
   Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -106,52 +134,66 @@ class MyApp extends StatelessWidget {
     );
   }
 
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
-          )
-        ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text("$_favoriteCount")
-          )
+    Color color = Theme.of(context).primaryColor;
+
+    return Container(
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButtonColumn(color, Icons.call, 'CALL'),
+              _buildButtonColumn(color, Icons.message, 'MESSAGE'),
+              _buildButtonColumn(color, Icons.email, 'EAMIL'),
+              _buildButtonColumn(color, Icons.share, 'SHARE'),
+              _buildButtonColumn(color, Icons.description, 'ETC'),
+            ]
         )
-      ],
     );
   }
 }
 
-class FavoriteWidget extends StatefulWidget {
+class BottomSection extends StatelessWidget {
   @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+  Widget build(BuildContext context) {
+
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Icon(
+            Icons.message,
+            color: Theme.of(context).primaryColor,
+            size: 40.0,
+          ),
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                      child: Text(
+                        'Recent Message',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                  ),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                      child: Text(
+                        'Long time no see!',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      )
+                  )
+                ],
+              )
+          ),
+        ],
+      ),
+    );
+  }
 }
-
-
-
