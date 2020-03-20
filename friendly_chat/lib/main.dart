abea2d9c6b5715f49302f271c48b3235a535b80f
SilverJun <jangej1031@naver.com>
2020-03-20 19:36:31 +0900

    W3-2 Classwork done.

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 const String _name = "Eunjun Jang";
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 void main() {
   runApp(FriendlychatApp());
@@ -10,6 +22,9 @@ class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: defaultTargetPlatform == TargetPlatform.iOS         //new
+          ? kIOSTheme                                              //new
+          : kDefaultTheme,
       title: "Friendlychat",
       home: ChatScreen(),
     );
@@ -31,7 +46,7 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     ChatMessage message = ChatMessage(
       text: text,
       animationController:  AnimationController(
-        duration: Duration(milliseconds: 700),
+        duration: Duration(milliseconds: 3000),       /// Modified
         vsync: this,
       ),
     );
@@ -63,7 +78,7 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
             Container(
               margin: EdgeInsets.symmetric(horizontal: 4.0),
               child: IconButton(
-                icon: Icon(Icons.send),
+                icon: Icon(_isComposing ? Icons.directions_run : Icons.directions_walk), /// Modified
                 onPressed: _isComposing ? () => _handleSubmitted(_textController.text) : null,
               ),
             )
@@ -78,25 +93,33 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         title: Text("Friendlychat"),
+        elevation:
+          Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              padding: EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
-            ),
+      body: Container(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
+              ),
+              Divider(height: 1.0),
+              Container(
+                decoration: BoxDecoration(color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              ),
+            ],
           ),
-          Divider(height: 1.0,),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor
-            ),
-            child: _buildTextComposer(),
-          )
-        ],
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Colors.grey[200]),
+                  ),
+              ) : null
       ),
     );
   }
@@ -135,10 +158,6 @@ class ChatMessage extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(right: 16.0),
-              child: CircleAvatar(child: Text(_name[0]),),
-            ),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -150,7 +169,12 @@ class ChatMessage extends StatelessWidget {
                   )
                 ],
               ),
-            )
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0]),),
+            ),
+
           ],
         ),
       ),
