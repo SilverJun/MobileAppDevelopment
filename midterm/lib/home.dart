f03148a60d51cadd5628b712dbf4df90b8b61f46
SilverJun <jangej1031@naver.com>
2020-03-26 10:47:31 +0900

    midterm mdc 01 running test done.

--- midterm/lib/home.dart
+++ midterm/lib/home.dart
@@ -14,6 +14,21 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:midterm/app.dart';
+class HomePage extends StatelessWidget {
+  // TODO: Make a collection of cards (102)
+  // TODO: Add a variable for Category (104)
 
-void main() => runApp(ShrineApp());
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return Scaffold(
+      // TODO: Add app bar (102)
+      // TODO: Add a grid view (102)
+      body: Center(
+        child: Text('You did it!'),
+      ),
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
