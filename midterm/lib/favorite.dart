a43b2ba77158125ed89b0bdfc38e639dca377834
SilverJun <jangej1031@naver.com>
2020-03-30 11:22:33 +0900

    midterm add pages

---
+++ midterm/lib/favorite.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Favorite'),),
+      body: Center(
+        child: Text('WebSitePage'),
+      ),
+    );
+  }
+}
---
+++ midterm/lib/website.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class WebSitePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text(''),),
+      body: Center(
+        child: Text('WebSitePage'),
+      ),
+    );
+  }
+}
