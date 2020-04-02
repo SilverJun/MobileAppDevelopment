ea87b76150dcb16c57a90aaf15b63aabe2d66d29
SilverJun <jangej1031@naver.com>
2020-04-02 15:28:58 +0900

    My page implementation done.

--- midterm/lib/mypage.dart
+++ midterm/lib/mypage.dart
@@ -1,14 +1,64 @@
 
 import 'package:flutter/material.dart';
+import 'package:midterm/HotelInfo.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:midterm/detailPage.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyPageState();
+}
+
+class MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My Page'),),
-      body: Center(
-        child: Text('MyPage'),
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 16.0,),
+          CarouselSlider(
+            viewportFraction: 0.9,
+            aspectRatio: 2.0,
+            autoPlay: true,
+            enlargeCenterPage: true,
+            items: hotelList.map( (hotel) {
+                return Container(
+                  margin: EdgeInsets.all(5.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(fullscreenDialog: true, builder: (context) => DetailPage(index: hotelList.indexOf(hotel),)));
+                          },
+                          child: Image.asset(
+                            hotel.image,
+                            fit: BoxFit.cover,
+                            width: 1000.0,
+                          ),
+                        ),
+                        Positioned(
+                          top: 4,
+                          child: Text(hotel.name, style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                        )
+                      ]
+                    ),
+                  ),
+                );
+              },
+            ).toList(),
+          ),
+        ]
       ),
     );
   }
 }
+
+/*
+onPressed: () {
+  Navigator.push(context, MaterialPageRoute(fullScreenDialog: true, builder: (context)=>DetailPage(index: hotelList.indexOf(product),)));
+}
+ */
