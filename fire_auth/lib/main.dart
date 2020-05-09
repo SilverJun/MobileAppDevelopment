0fac6ed88c78ec35ec5d5bc7c910b1d2b0be144b
SilverJun <jangej1031@naver.com>
2020-05-09 16:09:10 +0900

    firebase_auth done

---
+++ fire_auth/lib/main.dart
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './signin_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Auth Demo',
+      home: MyHomePage(title: 'Firebase Auth Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FirebaseUser user;
+  @override
+  Widget build(BuildContext context) {
+    return SignInPage();
+  }
+}
